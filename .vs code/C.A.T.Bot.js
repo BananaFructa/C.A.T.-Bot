c91b05efa20da9f7c40b9013f5a4a3c8fb730c20
Stefan Baltac <55049439+BananaFructa@users.noreply.github.com>
2019-11-09 20:45:03 +0200

    Add files via upload

--- .vs code/C.A.T.Bot.js
+++ .vs code/C.A.T.Bot.js
@@ -16,6 +16,7 @@ const REPfile = 'repLIST.txt';
 const MSGMINfile = 'msgminLIST.txt';
 const DATBSfile = 'datbsLIST.txt';
 const PREFIXfile = 'prefixLIST.txt';
+const TXTEVALLOWfile = 'txtevallowLIST.txt';
 const USNF = "User not found!";
 const defHTMLcolor = '#993399';
 const defImgURL = "https://imgur.com/";
@@ -50,7 +51,8 @@ const commands = [
     "stats",
     "rep",
     "cdb-help",
-    "prefix"
+    "prefix",
+    "txtevents"
 ];
 
 const brainCells = [
@@ -81,14 +83,16 @@ bot.on('ready', () => {
 bot.on('message', message => {
     if (!message.author.bot) {
         tryadduser(message.author.id);
-        //Message events
-        if (filterOut(message.content).includes('uwu') || filterOut(message.content).includes('owo')) {
-            message.channel.send('Sorry we do not do that here!');
-        } else if (filterOut(message.content).includes('cram')) {
-            message.channel.send(cramPhrases[Math.floor(Math.random() * cramPhrases.length)]);
-        } else if (filterOut(message.content).includes('bruh')) {
-            message.channel.send(bruhPhrases[Math.floor(Math.random() * bruhPhrases.length)]);
-            currentMsgBufferLimit = messageBufferLimit;
+        if (contained(TXTEVALLOWfile, message.guild.id + id_EXSYM) ? accesfile(TXTEVALLOWfile, message.guild.id + id_EXSYM) == 'on' : false) {
+            //Message events
+            if (filterOut(message.content).includes('uwu') || filterOut(message.content).includes('owo')) {
+                message.channel.send('Sorry we do not do that here!');
+            } else if (filterOut(message.content).includes('cram')) {
+                message.channel.send(cramPhrases[Math.floor(Math.random() * cramPhrases.length)]);
+            } else if (filterOut(message.content).includes('bruh')) {
+                message.channel.send(bruhPhrases[Math.floor(Math.random() * bruhPhrases.length)]);
+                currentMsgBufferLimit = messageBufferLimit;
+            }
         }
         var currentPrefix = contained(PREFIXfile, message.guild.id + id_EXSYM) ? accepted2normal(accesfile(PREFIXfile, message.guild.id + id_EXSYM)) : prefix;
         if (message.content.indexOf(currentPrefix) == 0) {
@@ -123,10 +127,11 @@ bot.on('message', message => {
                         commands[0] + " - Gives a random cat image from the bot's database \n" +
                         commands[2] + ' - See how many brain cells you have left \n' +
                         commands[7] + ' - Roll the dice \n' +
-                        commands[8] + ' - See how many times a command has been used \n' +
+                        commands[8] + ' - See how many times all commands have been used \n' +
                         commands[9] + " - View your or someone's else stats \n" +
                         commands[11] + " - Give someone a reputation point \n" +
                         commands[13] + " - Change the bot's  prefix for this guild \n" +
+                        commands[14] + " - Set the text events off or on (Administrator only , off by default) \n" +
                         commands[12] + " - Get the command list for the catpic database editor"
                     ));
                 incrementfile(LISTfile, commands[1], 1);
@@ -334,6 +339,33 @@ bot.on('message', message => {
                 }
                 reply = new Discord.RichEmbed().setColor(defHTMLcolor).setTitle("Succesfuly set prefix " + setPrefix + " for this current guild!");
                 message.channel.send(reply);
+                incrementfile(LISTfile, commands[13], 1);
+            } else if (commandContent.includes(commands[14])) {
+                var setTo = commandContent.replace(commands[14] + ' ', '');
+                var guildID = message.guild.id;
+                var reply;
+                if (setTo == 'on') {
+                    if (!contained(TXTEVALLOWfile, guildID + id_EXSYM)) {
+                        newentry(TXTEVALLOWfile, guildID + id_EXSYM, 'on');
+                        reply = new Discord.RichEmbed().setColor(defHTMLcolor).setTitle("Succesfuly set the text events to on!");
+                    } else if (accesfile(TXTEVALLOWfile, guildID + id_EXSYM) == 'on') {
+                        reply = new Discord.RichEmbed().setColor(defHTMLcolor).setTitle("The text events are already turned on!");
+                    } else if (accesfile(TXTEVALLOWfile, guildID + id_EXSYM) == 'off') {
+                        setfile(TXTEVALLOWfile, guildID + id_EXSYM, 'on');
+                        reply = new Discord.RichEmbed().setColor(defHTMLcolor).setTitle("Succesfuly set the text events to on!");
+                    }
+                } else if (setTo == 'off') {
+                    if (!contained(TXTEVALLOWfile, guildID + id_EXSYM)) {
+                        reply = new Discord.RichEmbed().setColor(defHTMLcolor).setTitle("The text events are already turned off!");
+                    } else if (accesfile(TXTEVALLOWfile, guildID + id_EXSYM) == 'off') {
+                        reply = new Discord.RichEmbed().setColor(defHTMLcolor).setTitle("The text events are already turned off!");
+                    } else if (accesfile(TXTEVALLOWfile, guildID + id_EXSYM) == 'on') {
+                        setfile(TXTEVALLOWfile, guildID + id_EXSYM, 'off');
+                        reply = new Discord.RichEmbed().setColor(defHTMLcolor).setTitle("Succesfuly set the text events to off!");
+                    }
+                }
+                message.channel.send(reply);
+                incrementfile(LISTfile, commands[14], 1);
             }
         }
         //inc message
