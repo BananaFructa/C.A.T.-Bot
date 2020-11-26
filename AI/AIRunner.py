5a991651e198dca8afa4c5e75c3765a6d3427593
Banana Fructa <55049439+BananaFructa@users.noreply.github.com>
2020-11-26 17:01:17 +0200

    added a delay in the python script

--- AI/AIRunner.py
+++ AI/AIRunner.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import keras as kr
 import numpy as np
 import matplotlib.image as mplimg
+import time
 
 CatGenerator = kr.models.load_model("./SavedAIs/CatGenerator")
 
@@ -12,6 +13,7 @@ def RunOne(ID):
          mplimg.imsave("./AI/OUT.jpg",tf.reshape(CatGenerator(np.random.randn(1,100)),(64,64,3)).numpy())
 
 while True:
+    time.sleep(0.1)
     try:
         RunOne(input())
         print("EXEC_SUCCEDED")
